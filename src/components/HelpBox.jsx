3175120d3fad258636ef4341a99258da9094afeb
Mani <mani30023@gmail.com>
2026-02-17 01:49:14 +0530

    initial commit

---
+++ src/components/HelpBox.jsx
@@ -0,0 +1,12 @@
+import './HelpBox.css';
+
+function HelpBox({ title, text }) {
+  return (
+    <article className="help-box">
+      <h3>{title}</h3>
+      <p>{text}</p>
+    </article>
+  );
+}
+
+export default HelpBox;
